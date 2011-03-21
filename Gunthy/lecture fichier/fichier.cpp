21f9a46d483ac8692a18463954ceba060a6326b1
anthony.bucquet <anthony.bucquet@ba17405d-034d-13a2-5758-f382d94d8804>
2011-03-21 15:51:24 +0000

    Ajout de la plupart des fonctions pour le fichier. Il manque la fonction nouvelle_ligne et les commandes à effectuer

--- Gunthy/lecture fichier/fichier.cpp
+++ Gunthy/lecture fichier/fichier.cpp
@@ -18,17 +18,95 @@ int aller_fin_de_ligne(FILE* fichier)
       return 2;
 }
 
-int charger_maille(char* nom_du_fichier, Maille* maille);
+int charger_maille(char* nom_du_fichier, Maille* maille)
 {
     FILE* fichier = ouvrir_fichier(nom_du_fichier);
-    detection_mot(fichier,cell_leng)
+    if(fichier == NULL)
+        return ERR_OUVERTURE_FICHIER;
 
+    while(nouvelle_ligne(fichier, maille)) {}  // Nouvelle ligne renvoie 0 si end of file
 
-
-    fermeture_fichier(fnom_du_fichier);
+    fermeture_fichier(fichier);
+    return 0;
 }
 
 void fermeture_fichier(FILE* nom_du_fichier)
 {
     fclose(nom_du_fichier);
+}
+
+int detection_mot(FILE* fichier, char* a_detecter)
+{
+    int i = 0;
+    int retour = 1; // trouve , non fin de ligne
+    char caractere = -1;
+
+    while(retour == 1 && a_detecter[i] != '\0')
+    {
+        caractere = lire_lettre(fichier);
+        if(caractere == '\n')   // Fin de ligne
+        {
+            retour = 2;
+        }
+        else if(caractere  != a_detecter[i])
+        {
+            retour = 0;
+        }
+        else if(caractere != ' ')
+            i++;
+    }
+
+    return retour;
+}
+
+int detection_reel(FILE* fichier, double* reel)
+{
+    double virgule = 0;
+    *reel = 0;
+    int retour = 0; // Non trouve, non fin de ligne
+    char caractere = 0;
+    int sortie = 0;
+
+    while(!sortie)
+    {
+       caractere = lire_lettre(fichier);
+       // 4 cas : chiffre, virgule, fin de ligne, autre
+       if(caractere == '\n')
+       {
+           retour+= 2;
+           sortie = 1;
+       }
+       else if(caractere == '.')
+       {
+            if(virgule == 0)
+                virgule = 1;
+            else
+            {
+                sortie = 1; // On sort de la, pas de deux virgules
+            }
+       }
+       else if(caractere>= 48 && caractere<= 57)// un chiffre
+       {
+            retour = 1; // on a au moins un chiffre
+            double chiffre = (double) (caractere - 48);
+            if(virgule == 0)
+            {
+                *reel = (*reel) * 10 + chiffre;
+            }
+            else
+            {
+                virgule /= 10;
+                *reel += (chiffre * virgule);
+            }
+       }
+       else // le nawak
+       {
+            if(retour == 1) // on a detecte au moins un chiffre
+            {
+                sortie = 1; // On sort de la
+            }
+       }
+    }
+
+    return retour;
 }
--- Gunthy/lecture fichier/fichier.h
+++ Gunthy/lecture fichier/fichier.h
@@ -6,20 +6,25 @@
 
 #include "maille.h"
 
-// Traitement de base du fichier
+// Les defines
+#define ERR_OUVERTURE_FICHIER 1
+
+
+// Traitement de base du fichier DONE
 FILE* ouvrir_fichier(char* nom_du_fichier); // ouvre le fichier dont l'adresse est nom_du_fichier
 void fermeture_fichier(FILE* fichier); // ferme le fichier
 char lire_lettre(FILE* fichier); // fonction qui renvoie la prochaine lettre et passe a la suivante (le passage a la lettre suivante se fait automatiquement)
 
-// Automate
+// Automate DONE
 // Retour par defaut : 0 si non trouve et non fin de ligne, 1 si trouve et non fin de ligne, 2 si non trouve et fin de ligne, 3 si trouve et fin de ligne
 int detection_reel(FILE* fichier, double* reel); // Le curseur est cense etre deja avant le reel. Si le reel n'est pas detecte, la fonction renvoie 0, 1 si le reel est detecte. Le reel est stocke dans *reel.
 int detection_mot(FILE* fichier, char* a_detecter); // Renvoie le retour par defaut
-int aller_fin_de_ligne(FILE* file); // Renvoie le retour par defaut
+int aller_fin_de_ligne(FILE* fichier); // Renvoie le retour par defaut
 
-void nouvelle_ligne(FILE* fichier, Maille* maille); // Nouvelle ligne = Nouvelle commande
+int nouvelle_ligne(FILE* fichier, Maille* maille, int modified); // Nouvelle ligne = Nouvelle commande | Renvoie 0 si end of file
+// modified est un parametre pour chaque champ modifie, un bit egal a 1 si modifie, 0 sinon  [IDEE]
 
 // Fonction finale a appeler
-int charger_maille(char* nom_du_fichier, Maille* maille); // Renvoie 0 si pas d'erreur
+int charger_maille(char* nom_du_fichier, Maille* maille); // Renvoie 0 si pas d'erreur Almost DONE
 
 #endif // FICHIER_H_INCLUDED
