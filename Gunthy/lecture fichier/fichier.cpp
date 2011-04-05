b8803d6cd45667ff9f12e12737c800f0834c8d5c
anthony.bucquet <anthony.bucquet@ba17405d-034d-13a2-5758-f382d94d8804>
2011-04-05 13:04:16 +0000

    Le code a été simplifié et il lit le paramètre "a" de la maille.

--- Gunthy/lecture fichier/fichier.cpp
+++ Gunthy/lecture fichier/fichier.cpp
@@ -7,15 +7,17 @@ FILE* ouvrir_fichier(char* nom_du_fichier)
 
 char lire_lettre(FILE* fichier)
 {
-     char c;
-     fscanf(fichier, "%c", &c);
+     char c = fgetc(fichier);
      return c;
 }
 
 int aller_prochaine_commande(FILE* fichier)
 {
-      while(lire_lettre(fichier)!='_') {}
-      return 2;
+    char c = lire_lettre(fichier);
+    while(c !='_' && c != EOF)
+        c = lire_lettre(fichier);
+
+    return (c == '_') ? 2 : 6;  // Nouvelle commande ou fin du fichier
 }
 
 int charger_maille(char* nom_du_fichier, Maille* maille)
@@ -35,13 +37,13 @@ void fermeture_fichier(FILE* nom_du_fichier)
     fclose(nom_du_fichier);
 }
 
-int detection_mot(FILE* fichier, char* a_detecter)
+int detection_mot(FILE* fichier, const char* a_detecter)
 {
     int i = 0;
     int retour = 1; // trouve , non fin de ligne
     char caractere = -1;
 
-    while(retour == 1 && a_detecter[i] != '\0')
+    while(retour%4 == 1 && a_detecter[i] != '\0')
     {
         caractere = lire_lettre(fichier);
         if(caractere == '_')   // nouvelle commande
@@ -54,8 +56,9 @@ int detection_mot(FILE* fichier, char* a_detecter)
         }
         else if(caractere != ' ')
             i++;
+        else if(caractere == EOF)
+            retour = 4;
     }
-
     return retour;
 }
 
@@ -70,7 +73,7 @@ int detection_reel(FILE* fichier, double* reel)
     while(!sortie)
     {
        caractere = lire_lettre(fichier);
-       // 4 cas : chiffre, virgule, fin de ligne, autre
+       // 5 cas : chiffre, virgule, nouvelle commande, EOF, autre
        if(caractere == '_')
        {
            retour+= 2;
@@ -99,6 +102,11 @@ int detection_reel(FILE* fichier, double* reel)
                 *reel += (chiffre * virgule);
             }
        }
+       else if(caractere == EOF)
+       {
+           retour += 4; // bye bye
+           sortie = 1;
+       }
        else // le nawak
        {
             if(retour == 1) // on a detecte au moins un chiffre
@@ -121,185 +129,142 @@ int automate(FILE* fichier, Maille* maille)
     {
         switch(etat)
         {
-            case NOUVELLE_COMMANDE:                         // l'underscore est passe, toute nouvelle commande
-                c = lire_lettre(fichier);
-
-                switch(c)
-                {
-
-
-                    case EOF :
-                        etat = FIN_FICHIER;
-                    break;
-
-
-                    case 'c' :
-                        retour = detection_mot(fichier, "ell");
-                        if(retour%4 == 0)
-                            etat = ATTENTE_NVELLE_COMMANDE;
-                        else if(retour%4 == 1)
-                        {
-                            aller_prochaine_commande(fichier);
-                            etat = CELL;
-                        }
-                        else if(retour%4 == 2)
-                            etat = NOUVELLE_COMMANDE;
-                        else
-                            etat = CELL;
-
-                        if(retour/4 == 1) // End of file
-                            etat = FIN_FICHIER;
-                    break;
-
-
-                    case 's' :
-                        retour = detection_mot(fichier, "ymmetry");
-                        if(retour%4 == 1)
-
-                             etat = ATTENTE_NVELLE_COMMANDE;
-                        else if(retour%4 == 1)
-                        {
-                            aller_prochaine_commande(fichier);
-                            etat = SYMMETRY;
-                        }
-                        else if(retour%4 == 2)
-                            etat = NOUVELLE_COMMANDE;
-                        else
-                            etat = SYMMETRY;
-
-                        if(retour/4 == 1) // End of file
-                            etat = FIN_FICHIER;
-                    break;
-
-
-                    case 'a' :
-                        retour = detection_mot(fichier, "tome");
-                        if(retour%4 == 1)
-
-                             etat = ATTENTE_NVELLE_COMMANDE;
-                        else if(retour%4 == 1)
-                        {
-                            aller_prochaine_commande(fichier);
-                            etat = ATOME;
-                        }
-                        else if(retour%4 == 2)
-                            etat = NOUVELLE_COMMANDE;
-                        else
-                            etat = ATOME;
-
-                        if(retour/4 == 1) // End of file
-                            etat = FIN_FICHIER;
-                    break;
-
-
-
-                    default :
-                        etat = ATTENTE_NVELLE_COMMANDE;
-                }
+            case ATTENTE_NVELLE_COMMANDE :
+                if(aller_prochaine_commande(fichier) == 2)
+                    etat = NOUVELLE_COMMANDE;
+                else
+                    etat = FIN_FICHIER;
             break;
 
+            case NOUVELLE_COMMANDE:           // l'underscore est passe, toute nouvelle commande
+                etat_nouvelle_commande(fichier, maille, &etat, &retour, &c);
+            break;
 
-         case CELL:
-            c = lire_lettre(fichier);
-              if (c != '_')
-             {etat = NOUVELLE_COMMANDE;}
+            case CELL :
+                etat_cell(fichier, maille, &etat, &retour, &c);
+            break;
 
+            case CELL_LENGTH:
+                etat_cell_length(fichier, maille, &etat, &retour, &c);
+            break;
 
-              else
-              {
-
-               c = lire_lettre(fichier);
-                switch (c)
-
-
-                case 'a' :
-                    retour = detection_mot(fichier, "ngle");
-                    if(retour%4 == 1)
-
-                                 {etat = ATTENTE_NVELLE_COMMANDE;}
-                    else if(retour%4 == 1)
-                            {
-                                aller_prochaine_commande(fichier);
-                                etat = CELL_ANGLE;
-                            }
-                    else if(retour%4 == 2)
-                                etat = NOUVELLE_COMMANDE;
-                    else
-                                etat = CELL_ANGLE;
-
-                    if(retour/4 == 1) // End of file
-                                etat = FIN_FICHIER;
-                break;
-
-
-                case 'l' :
-                    retour = detection_mot(fichier, "ength");
-                    if(retour%4 == 1)
-
-                                 etat = ATTENTE_NVELLE_COMMANDE;
-                    else if(retour%4 == 1)
-                            {
-                                aller_prochaine_commande(fichier);
-                                etat = CELL_LENGTH;
-                            }
-                    else if(retour%4 == 2)
-                                etat = NOUVELLE_COMMANDE;
-                    else
-                                etat = CELL_LENGTH;
-
-                    if(retour/4 == 1)                            // End of file
-                                etat = FIN_FICHIER;
-                break;
-
-
-                default :
-                        etat = ATTENTE_NVELLE_COMMANDE;
-
-                break;
-            }
-            break;                                       //fermeture case CELL
-
-
-
-        case CELL_LENGTH:
-            c = lire_lettre(fichier);
-
-              if (c != '_')
-             {etat = NOUVELLE_COMMANDE;}
-
-
-              else
-              { c = lire_lettre(fichier);
-
-                switch (c)  {
-                    case 'a' :
-                        retour=detection_reel(fichier, &(maille->a));
-                        if(retour%4 == 1)
-
-                                     etat = ATTENTE_NVELLE_COMMANDE;
-                        else if(retour%4 == 1)
-                                {
-                                    aller_prochaine_commande(fichier);
-                                    etat = NOUVELLE_COMMANDE;
-                                }
-                        else if(retour%4 == 2)
-                                    etat = NOUVELLE_COMMANDE;
-
-                        if(retour/4 == 1)                            // End of file
-                                    etat = FIN_FICHIER;
-                    break;
-
-                }
-
-
-
-
-
-
-              }
-
+            default :
+                etat = ATTENTE_NVELLE_COMMANDE;
+            break;
         }
+
     }
 
-    return 0;
+    return 0;   // A CHANGER
+}
+
+void traiter_sous_commande(FILE* fichier, Maille* maille, int* etat, int* retour, char* c, const char* a_detecter, int etat_a_atteindre)
+{
+    *retour = detection_mot(fichier, a_detecter);
+    if(*retour/4 == 1) // End of file
+    {
+        *etat = FIN_FICHIER;
+        return;
+    }
+
+    if(*retour%4 == 0)       // Non trouve non fin de ligne : cas classique 1
+        *etat = ATTENTE_NVELLE_COMMANDE;
+    else if(*retour%4 == 1) // Trouve et non fin de ligne
+    {
+        if(aller_prochaine_commande(fichier) == 2)
+            *etat = etat_a_atteindre;
+        else
+            *etat = FIN_FICHIER;
+    }
+    else if(*retour%4 == 2)                 // Non trouve mais on a passe l underscore
+        *etat = NOUVELLE_COMMANDE;
+    else
+        *etat = etat_a_atteindre;           // On a trouve et passe l underscore : cas classique 2
+}
+
+void etat_nouvelle_commande(FILE* fichier, Maille* maille, int* etat, int* retour, char* c)
+{
+    *c = lire_lettre(fichier);
+    switch(*c)
+    {
+        case EOF :
+            *etat = FIN_FICHIER;
+        break;
+
+        case 'c' :
+            traiter_sous_commande(fichier, maille, etat, retour, c, "ell", CELL);
+        break;
+
+        case 's' :
+            traiter_sous_commande(fichier, maille, etat, retour, c, "ymmetry", SYMMETRY);
+        break;
+
+        case 'a' :
+            traiter_sous_commande(fichier, maille, etat, retour, c, "tome", ATOME);
+        break;
+
+        case '_' :
+            *etat = NOUVELLE_COMMANDE;
+        break;
+
+        default :
+            *etat = ATTENTE_NVELLE_COMMANDE;
+        break;
+    }
+}
+
+void etat_cell(FILE* fichier, Maille* maille, int* etat, int* retour, char* c)
+{
+   *c = lire_lettre(fichier);
+    switch (*c)
+    {
+        case EOF :
+            *etat = FIN_FICHIER;
+        break;
+
+        case 'a' :
+            traiter_sous_commande(fichier, maille, etat, retour, c, "ngle", CELL_ANGLE);
+        break;
+
+        case 'l' :
+            traiter_sous_commande(fichier, maille, etat, retour, c, "ength", CELL_LENGTH);
+        break;
+
+        case '_' :
+            *etat = NOUVELLE_COMMANDE;
+        break;
+
+        default :
+            *etat = ATTENTE_NVELLE_COMMANDE;
+        break;
+    }
+}
+
+void etat_cell_length(FILE* fichier, Maille* maille, int* etat, int* retour, char* c)
+{
+     *c = lire_lettre(fichier);
+
+    switch (*c)
+    {
+        case EOF :
+            *etat = FIN_FICHIER;
+        break;
+
+        case 'a' :
+            *retour = detection_reel(fichier, &(maille->a));
+            if(*retour%4 <= 1)         // On n'a pas atteint l underscore
+                *etat = ATTENTE_NVELLE_COMMANDE;
+            else                       // On a atteint l'underscore
+                *etat = NOUVELLE_COMMANDE;
+            if(*retour/4 == 1)
+                *etat = FIN_FICHIER;
+        break;
+
+        case '_' :
+            *etat = NOUVELLE_COMMANDE;
+        break;
+
+        default :
+            *etat = ATTENTE_NVELLE_COMMANDE;
+    }
 }
--- Gunthy/lecture fichier/main.cpp
+++ Gunthy/lecture fichier/main.cpp
@@ -3,8 +3,10 @@
 int main ()
 {
     Maille m;
-    charger_maille("1000001.cif",&m);
-    printf ("%lf",m.a);
+    int retour = charger_maille("1000001.cif",&m);
+    if(retour == ERR_OUVERTURE_FICHIER)
+        printf("Erreur a l ouverture du fichier\n");
+    printf ("Parametre a : %lf\n",m.a);
 
     return 0;
 }
