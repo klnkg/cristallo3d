e5629bf6e3bb08378f47472641ffb7931b99a40b
anthony.bucquet <anthony.bucquet@ba17405d-034d-13a2-5758-f382d94d8804>
2011-04-11 00:22:02 +0000

    Ajout de sources pour les révisions du 11 Avril

---
+++ trunk/Revisions/bug3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    printf("Ce programme ne compile pas\n");
+
+    int i;
+    for(i=0; i<9; i++)
+    {
+        printf("%ld\n", i)
+    }
+
+
+    return 0;
+}
