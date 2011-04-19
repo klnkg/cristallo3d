47cf1ea36c4e1724a9d85ce7bac3cb7df12d102f
anthony.bucquet <anthony.bucquet@ba17405d-034d-13a2-5758-f382d94d8804>
2011-04-19 13:34:16 +0000

    Ajout de l'octree

---
+++ Code final/octree.cpp
@@ -0,0 +1,41 @@
+#include "Octree.h"
+
+int position_octree(Point origine, Point M)
+{
+    int retour = 0;
+    if(origine.x < M.x)
+        retour += 4;
+    if(origine.y < M.y)
+        retour += 1;
+    if(origine.z < M.z)
+        retour += 2;
+
+    return retour;
+}
+
+Octree* nouv_element(Point M)
+{
+    Octree* retour = (Octree*) malloc(sizeof(Octree));
+        retour->M = M;
+    int i;
+    for(i=0; i<8; i++)
+        retour->fils[i] = NULL;
+
+    return retour;
+}
+
+int ajouter_a_l_affichage(L_affiche* l, Atome a)
+{
+    if(*l == NULL)
+    {
+        *l = nouv_element(a.position);
+        return 1;
+    }
+    else if(egalite(a.position, (*l)->M))
+        return 0;
+    else
+    {
+        int fils = position_octree((*l)->M, a.position);
+        return ajouter_a_l_affichage(&((*l)->fils[fils]), a);
+    }
+}
