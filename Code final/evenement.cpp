14d8582bf0b6841762ce043f74f4fb760a88b0f8
anthony.bucquet <anthony.bucquet@ba17405d-034d-13a2-5758-f382d94d8804>
2011-04-27 22:20:42 +0000

    Le premier slider marche

--- Code final/evenement.cpp
+++ Code final/evenement.cpp
@@ -106,8 +106,7 @@ void evenement_bouton(HWND handle, UINT message, WPARAM wParam, LPARAM lParam)
         case ID_RETRO : action_activer_retro(SendMessage(g_fenetre->retro, BM_GETCHECK, 0, 0) == BST_CHECKED); break;
         case ID_T_RETRO :
             if(HIWORD(wParam) == EN_CHANGE)
-            id;
-              //  action_change_edit(g_fenetre->t_retro, g_fenetre->s_retro, 1, 0.1, 100.);
+                action_change_edit(g_fenetre->t_retro, g_fenetre->s_retro, 1, 0.1, 50.);
             if(HIWORD(wParam) == EN_UPDATE)
                 action_update_edit(g_fenetre->t_retro);
              break;
@@ -120,5 +119,5 @@ void evenement_bouton(HWND handle, UINT message, WPARAM wParam, LPARAM lParam)
     }
 
     if((HWND)lParam == g_fenetre->s_retro)// && LOWORD(message) == TBM_SETPOS)
-        action_change_edit(g_fenetre->t_retro, g_fenetre->s_retro, 0, 0.1, 100.);
+        action_change_edit(g_fenetre->t_retro, g_fenetre->s_retro, 0, 0.1, 50.);
 }
