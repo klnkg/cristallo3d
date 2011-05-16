10551c5a784975487715f2576596a5a25e81c35a
anthony.bucquet <anthony.bucquet@ba17405d-034d-13a2-5758-f382d94d8804>
2011-05-16 01:11:30 +0000

    Nouvelle avancée dans le chargement de la maille. Passage de la premaille à la maille. Reste à faire le chargement de l'octree et de finaliser ça

--- trunk/Code final/affichage_maille.cpp
+++ trunk/Code final/affichage_maille.cpp
@@ -1,32 +1,32 @@
-#include "affichage_maille.h"
+#include <math.h>
 
-
-void test_affichage(int affiche[9], Camera* C, Octree* O)
+test_affichage(int affiche[9], Camera* C, Octree* O)
 {
-    int i;
-    for (i=0; i<9; i++) affiche[i]=1;
-
-    if (prod_scal(C->z, {1,0,0})<0 & C->origine.x<O->M.position.x)
+    if (prod_scal(Camera.z, {1,0,0})<0 & Camera.origine.x<Octree.M.position.x)
     {
+        int i;
         for (i=4; i<9; i++) affiche[i]=0; //annule les x positifs
     }
-    if (prod_scal(C->z, {1,0,0})>0 & C->origine.x>O->M.position.x)
+    if (prod_scal(Camera.z, {1,0,0})>0 & Camera.origine.x>Octree.M.position.x)
     {
         affiche[8]=0;
+        int i;
         for (i=0; i<4; i++) affiche[i]=0; //annule les x négatifs
     } //test sur x
 
-    if (prod_scal(C->z, {0,1,0})<0 & C->origine.y<O->M.position.y)
+    if (prod_scal(Camera.z, {0,1,0})<0 & Camera.origine.y<Octree.M.position.y)
+    {
+        int i;
+        for (i=0; i<5; i++) affiche[2i]=0; //annule les y positifs
+    }
+    if (prod_scal(Camera.z, {0,1,0})>0 & Camera.origine.y>Octree.M.position.y)
     {
         affiche[8]=0;
-        for (i=0; i<4; i++) affiche[2*i+1]=0; //annule les y positifs
-    }
-    if (prod_scal(C->z, {0,1,0})>0 & C->origine.y>O->M.position.y)
-    {
-        for (i=0; i<5; i++) affiche[2*i]=0; //annule les y négatifs
+        int i;
+        for (i=0; i<4; i++) affiche[2i+1]=0; //annule les y négatifs
     } //test sur y
 
-    if (prod_scal(C->z, {0,0,1})<0 & C->origine.z<O->M.position.z)
+    if (prod_scal(Camera.z, {0,0,1})<0 & Camera.origine.z<Octree.M.position.z)
     {
         affiche[2]=0;
         affiche[3]=0;
@@ -34,7 +34,7 @@ void test_affichage(int affiche[9], Camera* C, Octree* O)
         affiche[7]=0;
         affiche[8]=0;//annule les z positifs
     }
-    if (prod_scal(C->z, {0,0,1})>0 & C->origine.z>O->M.position.z)
+    if (prod_scal(Camera.z, {0,0,1})>0 & Camera.origine.z>Octree.M.position.z)
     {
         affiche[0]=0;
         affiche[1]=0;
@@ -44,42 +44,37 @@ void test_affichage(int affiche[9], Camera* C, Octree* O)
     } //test sur z
 }
 
-void afficher_atome(Atome A, Atome_Type T)
-{
-
-    Sphere sphere;
-    sphere.radius=T.rayon_ionique;
-    sphere.slices=100;
-    sphere.stacks=100;
-
-    Point centre;
-    centre.x=A.position.x;
-    centre.y=A.position.y;
-    centre.z=A.position.z;
-
-    dessiner_sphere(sphere, couleurs[T.index_couleur], centre);
-}
-
-void afficher_maille (Maille* M, Camera* C){_afficher_maille(M, C, M->octree);}
-
-void _afficher_maille(Maille* M, Camera* C, Octree* O) // O est le premier sommet
+afficher_maille(Maille* M, Camera* C, Octree* O) // O est le premier sommet
 {
     if (O==NULL) return;
-    else
+    glPushMatrix();
+
+    glColor3ub(couleur.r,couleur.v,couleur.b);
+
+    int affiche[9];
+    int i;
+    for (i=0; i<9; i++) affiche[i]=1
+    test_affichage(affiche[9], &C, &O);
+    if (affiche[8]==1)
     {
-        int affiche[9];
-        test_affichage(affiche, C, O);
-        if (affiche[8]==1)
+        afficher_maille(&M, &C, &O)
+        int j;
+        for (j=0; j<8; j++)
         {
-            Atome A=O->M;
-            Atome_Type T=M->types[A.type];
-            afficher_atome(A, T);
-            int j;
-            for (j=0; j<8; j++)
-            {
-                if (affiche[j]==1) _afficher_maille(M, C, O->fils[j]);
-            }
+            if (affiche[i]==1) afficher_maille(&M, &C, &(O->Fils[i]))
         }
     }
 
+    dessiner_sphere()
+
+    int i;
+    for (i=0; i<8; i++)
+    {
+        if (devant_camera)
+        afficher_maille(O->Fils[i])
+    }
+    afficher_maille(O->Fils[i])
+
+    glEnd();
+    glPopMatrix();
 }
